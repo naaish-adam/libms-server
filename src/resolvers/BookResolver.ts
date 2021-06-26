d9b17e1838627e5a189131a5a58bfac41654af36
naaish-adam <adamnaaish@gmail.com>
2021-06-26 20:39:45 +0500

    FEAT: Books findAll using queryBuilder and new filters

--- src/resolvers/BookResolver.ts
+++ src/resolvers/BookResolver.ts
@@ -1,37 +1,51 @@
-import { Book } from "../entities/Book";
 import {
   Arg,
   Args,
   Field,
+  FieldResolver,
   InputType,
+  Int,
   Mutation,
   Query,
   Resolver,
+  Root,
 } from "type-graphql";
-import { connectionFromArraySlice } from "../pagination/ConnectionArgs";
+import { getConnection } from "typeorm";
+
+import { Book } from "../entities/Book";
+import { Copy, CopyStatus } from "../entities/Copy";
+
 import { BooksConnectionArgs, PaginatedBooks } from "../pagination/Books";
-import { ILike } from "typeorm";
+import { connectionFromArraySlice } from "../pagination/ConnectionArgs";
 
 @InputType()
 class BookInput {
-  @Field({ nullable: true })
-  isbn: number;
-
-  @Field()
-  name: string;
-
-  @Field()
-  author: string;
-
-  @Field()
-  publishedDate: Date;
+  @Field(() => Int, { nullable: true })
+  id?: number;
 
   @Field({ nullable: true })
-  cover: string;
+  isbn?: string;
+
+  @Field()
+  name!: string;
+
+  @Field()
+  author!: string;
+
+  @Field()
+  publishedDate!: Date;
+
+  @Field({ nullable: true })
+  cover?: string;
 }
 
-@Resolver()
+@Resolver(Book)
 export class BookResolver {
+  @FieldResolver()
+  async copies(@Root() book: Book) {
+    return await Copy.find({ where: { book } });
+  }
+
   @Query(() => PaginatedBooks)
   async books(@Args() args: BooksConnectionArgs): Promise<PaginatedBooks> {
     const { limit, offset } = args.pagingParams();
@@ -41,25 +55,49 @@ export class BookResolver {
     const realLimit = Math.min(50, limit || 50);
     const realLimitPlusOne = realLimit + 1;
 
-    const where = filter?.searchTerm
-      ? [
-          {
-            name: ILike(`%${filter.searchTerm}%`),
-          },
-          {
-            author: ILike(`%${filter.searchTerm}%`),
-          },
-          {
-            isbn: ILike(`%${filter.searchTerm}%`),
-          },
-        ]
-      : undefined;
+    const query = getConnection()
+      .createQueryBuilder(Book, "book")
+      .leftJoinAndSelect("book.copies", "copy");
 
-    const [bookList, count] = await Book.findAndCount({
-      skip: offset,
-      take: realLimitPlusOne,
-      where,
-    });
+    if (filter?.searchTerm) {
+      query
+        .where("book.name like :name", { name: `%${filter.searchTerm}%` })
+        .orWhere("book.author like :author", {
+          author: `%${filter.searchTerm}%`,
+        })
+        .orWhere("book.isbn like :isbn", { isbn: `%${filter.searchTerm}%` });
+    }
+
+    if (filter?.onlyAvailable) {
+      query.andWhere("copy.status = :status", { status: CopyStatus.AVAILABLE });
+    }
+
+    const [bookList, count] = await query
+      .skip(offset)
+      .take(realLimitPlusOne)
+      .orderBy("book.createdAt", "DESC")
+      .getManyAndCount();
+
+    // const where = filter?.searchTerm
+    // ? [
+    //     {
+    //       name: ILike(`%${filter.searchTerm}%`),
+    //     },
+    //     {
+    //       author: ILike(`%${filter.searchTerm}%`),
+    //     },
+    //     {
+    //       isbn: ILike(`%${filter.searchTerm}%`),
+    //     },
+    //   ]
+    // : undefined;
+
+    // const [bookList, count] = await Book.findAndCount({
+    //   skip: offset,
+    //   take: realLimitPlusOne,
+    //   where,
+    //   order: { createdAt: "DESC" },
+    // });
 
     const { edges, pageInfo } = connectionFromArraySlice(
       bookList.slice(0, realLimit),
@@ -86,7 +124,19 @@ export class BookResolver {
   }
 
   @Mutation(() => Book)
-  async addBook(@Arg("bookInput") bookInput: BookInput) {
+  async addBook(@Arg("bookInput") bookInput: BookInput): Promise<Book> {
     return await Book.create(bookInput).save();
+  }
+
+  @Mutation(() => Book)
+  async updateBook(@Arg("bookInput") bookInput: BookInput): Promise<Book> {
+    await Book.update(bookInput.id as number, bookInput);
+    return (await Book.findOne(bookInput.id)) as Book;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteBook(@Arg("id", () => Int) id: number) {
+    await Book.delete(id);
+    return true;
   }
 }
